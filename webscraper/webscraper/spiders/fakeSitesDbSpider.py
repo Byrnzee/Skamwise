3fad94437f108d55b7da63725c5f538d08653407
Ciaran Byrne <byrneciaran11@gmail.com>
2022-08-06 21:12:51 +0100

    fixed bug with webcrawler

--- webscraper/webscraper/spiders/fakeSitesDbSpider.py
+++ webscraper/webscraper/spiders/fakeSitesDbSpider.py
@@ -5,15 +5,16 @@ class FakeSitesDbSpider(scrapy.Spider):
     start_urls = ['https://db.aa419.org/fakebankslist.php']                              # website to be web scraped
 
     def parse(self, response):
-        for row in response.css ('table.ewTable'):                                       # for every URL in table
-            list = row.css('table.ewTable tr td:nth-child(2) a::attr(href)').extract()   # extract URLs
+        for row in response.css ('table'):                                              # for every URL in table
+            list = row.css('table tr td:nth-child(2) a::text').extract()
+            print(list)# extract URLs
             for url in list:
                 yield{
                     'url': url
                 }
 
 
-        next_page = response.css('div[id="main"] a::attr(href)')[14].extract()          # go to the next page
+        next_page = response.css('div[class="pager"] a::attr(href)')[12].extract()          # go to the next page
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)                       # stop if theres no next page
 
